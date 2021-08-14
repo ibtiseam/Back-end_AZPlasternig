d1635e7687b7aa42eef7cb75020c5ba88b2f8e6b
Ibtiseam <ibtissem.bouzidii@gmail.com>
2021-08-14 14:01:10 +0200

    init commit

---
+++ src/main/java/com/example/AZplastering/AzPlasteringApplication.java
@@ -0,0 +1,13 @@
+package com.example.AZplastering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzPlasteringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzPlasteringApplication.class, args);
+	}
+
+}
